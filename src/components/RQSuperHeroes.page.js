932a013313076a4e3c55b1024f1bb1c9c6856152
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-29 23:51:47 +0100

    Add success & error handling

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useState } from "react";
 
 const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
 export const RQSSuperHeroesPage = () => {
+  const [polling, setPolling] = useState(3000);
+
+  const onSuccess = (data) => {
+    if (data.data.length === 4) {
+      setPolling(false);
+    }
+    console.log("Perform side effect after data fetching", data);
+  };
+
+  const onError = (error) => {
+    setPolling(false);
+    console.log("Perform side effect after encountering error", error);
+  };
+
   const { isError, data, refetch, isLoading, error, isFetching } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
@@ -14,9 +29,11 @@ export const RQSSuperHeroesPage = () => {
       // staleTime: 1000,
       // refetchOnMount: true,
       // refetchOnWindowFocus: true,
-      // refetchInterval: 2000,
+      // refetchInterval: polling,
       // refetchIntervalInBackground: true,
-      enabled: false,
+      // enabled: false,
+      onError,
+      onSuccess,
     }
   );
 
