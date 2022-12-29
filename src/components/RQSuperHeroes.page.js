9ac721195f6d6584dd39273bfcafd383e4887246
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-29 20:37:51 +0100

    Fetch data with useQuery

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSSuperHeroesPage = () => {
-  return <h2>React Query super heroes Page</h2>;
+  const { data, isLoading } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes</h2>
+      {data?.data.map((hero) => (
+        <div key={data.id}>{hero.name}</div>
+      ))}
+    </>
+  );
 };
