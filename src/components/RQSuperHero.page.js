a93fa4743bd2960d4b442b08f618daf6e63f19df
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-31 00:12:32 +0100

    Query by id

--- src/components/RQSuperHero.page.js
+++ src/components/RQSuperHero.page.js
@@ -1,7 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useSuperHeroData } from "./../hooks/useSuperHeroData";
+
 export const RQSuperHeroPage = () => {
+  const { heroId } = useParams();
+  const { data, isLoading, isError, error } = useSuperHeroData(heroId);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error}</h2>;
+  }
+
   return (
     <>
       <h2>Super Hero Page</h2>
+      <div>
+        {data?.data.name} - {data?.data.alterEgo}
+      </div>
     </>
   );
 };
